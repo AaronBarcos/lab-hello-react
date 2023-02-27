55fdf22c4350005b0d5c6e058ef54d128294f92b
AaronBarcos <abarcoscaballero@gmail.com>
2023-02-27 17:36:04 +0100

    lab done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
+import icon from "./images/lineas.png"
 
 function App() {
   return (
     <div className="App">
-      <section id='one' style={{backgroundColor: "black"}} >
+      <section id='one' style={{backgroundColor: "black", padding: "50px" }} >
         <div id='top'>
           <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png" alt="" />
-          <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png" alt="" />
+          <img src={icon} alt="lineas" width= "50px" height="50px" />
         </div>
         <div id='down'>
-          <h1>Say hello to ReactJs</h1>
-          <h4>You will learn how to use the most popular frontend library, and become a super Ninja developer.</h4>
-          <button>Awesome!</button>
+          <h1 style={{color: "white", fontSize: "50px", width: "300px"}}>Say hello to ReactJs</h1>
+          <h4 style={{color: "white", fontSize: "25px", width: "450px"}}>You will learn how to use <br></br> the most popular frontend library, and become a super Ninja developer.</h4>
+          <button style={{width: "200px", padding: "20px", fontSize: "25px"}} >Awesome!</button>
         </div>
       </section>
       <section id='two'>
@@ -34,7 +35,7 @@ function App() {
           <div>
             <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png" alt="" />
             <h3>JSX</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+            <p>Statically-typed, designed to run on modern browsers.</p>
           </div>
       </section>
     </div>
